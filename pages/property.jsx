a5b226884ffe315cc4dffb78bb4e65240759c7cf
Shannon Mettry <shannonmettry12@gmail.com>
2022-09-30 14:53:01 +0200

    setting up project page for Miguel website

---
+++ pages/property.jsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+import React from 'react'
+import propertyImg from "../public/assets/projects/property.png";
+
+function property() {
+  return (
+    <div className='w-full'>
+    <div className='w-screen h-[30vh] lg:h-[40vh] relative'>
+<div className='absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/80 z-10' />
+<Image className='absolute z-1' layout='fill' objectFit='cover' src={propertyImg} alt='/' />
+    </div>
+  </div>
+  )
+}
+
+export default property
